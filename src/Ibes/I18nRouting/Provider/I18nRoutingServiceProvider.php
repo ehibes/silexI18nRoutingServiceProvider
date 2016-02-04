db6a0db6fc3dcac1fc319cbbef07c003bb2da724
ehibes <ibes.mongabure@gmail.com>
2016-02-04 16:05:35 +0100

    Remove travis php 5.3 & 5.4 support for silex2

--- src/Ibes/I18nRouting/Provider/I18nRoutingServiceProvider.php
+++ src/Ibes/I18nRouting/Provider/I18nRoutingServiceProvider.php
@@ -5,11 +5,13 @@ namespace Ibes\I18nRouting\Provider;
 use Ibes\I18nRouting\I18nControllerCollection;
 use Ibes\I18nRouting\I18nUrlGenerator;
 use Silex\Application;
-use Silex\ServiceProviderInterface;
+use Pimple\Container;
+use Pimple\ServiceProviderInterface;
+
 
 class I18nRoutingServiceProvider implements ServiceProviderInterface
 {
-    public function register(Application $app)
+    public function register(Container $app)
     {
         $app['controllers_factory'] = function () use ($app) {
             return new I18nControllerCollection(
@@ -19,11 +21,10 @@ class I18nRoutingServiceProvider implements ServiceProviderInterface
             );
         };
 
-        $app['url_generator'] = $app->share(function ($app) {
+        $app['url_generator'] = function ($app) {
             $app->flush();
-
             return new I18nUrlGenerator($app['routes'], $app['request_context']);
-        });
+        };
 
         $app['i18n_routing.locales'] = array('en');
     }
